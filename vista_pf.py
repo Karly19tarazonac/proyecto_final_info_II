65f6d093eb9dc355d98f90a0c822a6da4bd2a013
Karly19tarazonac <133623213+Karly19tarazonac@users.noreply.github.com>
2023-11-26 15:23:31 -0500

    comentarios

--- vista_pf.py
+++ vista_pf.py
@@ -14,6 +14,7 @@ import nilearn
 from nilearn import plotting
 import shutil
 
+#ventana principal
 class Ventanaprincipal(QMainWindow):
     #constructor
     def __init__(self, ppal=None):
@@ -27,15 +28,17 @@ class Ventanaprincipal(QMainWindow):
         #se programa la senal para el boton
         self.login.clicked.connect(self.accion_ingresar)
     
+    #se llama al controlador
     def asignarControlador(self,c):
         self.__controlador = c
-
+    
+    #funcion de ingresar 
     def accion_ingresar(self):
         usuario = self.campo_usuario.text()
         password = self.campo_password.text()
-        #esta informacion la debemos pasar al controlador
+        #esta informacion la debemos pasar al controlador para que el modelo valide
         resultado = self.__controlador.validar_usuario(usuario,password)
-        #se crea la ventana de resultado
+        #se crea la ventana de resultado con la validacion
         msg = QMessageBox(self)
         msg.setIcon(QMessageBox.Information)
         msg.setWindowTitle("Resultado")
@@ -49,6 +52,7 @@ class Ventanaprincipal(QMainWindow):
         #se muestra la ventana
             msg.show()
 
+    #recibe la imagenes para conectarlas al modelo por medio del controlador
     def recibir_imagen(self,imagen):
         self.__controlador.img_conextion(imagen)
 
@@ -57,7 +61,8 @@ class Ventanaprincipal(QMainWindow):
     
     def recibir_imagen3d(self,imagen):
         self.__controlador.nifti_conversion(imagen)
-        
+    
+    #recibe informacion de los sliders    
     def slider_uno(self, event):
         return self.__controlador.datos_slide1(event)
         
@@ -66,7 +71,7 @@ class Ventanaprincipal(QMainWindow):
     
     def slider_tres(self, event):
         return self.__controlador.datos_slide3(event)
-        
+    #abre ventana de opciones   
     def abrirVentanaopciones(self):
         ventana_opciones=AbrirVentana_opciones(self)
         self.hide()
@@ -76,14 +81,16 @@ class Ventanaprincipal(QMainWindow):
         ventana_opciones=Ventana_opciones2(self)
         self.hide()
         ventana_opciones.show()
-    
+
+#ventana de opcion para medicos   
 class AbrirVentana_opciones(QDialog):
     def __init__(self, ppal=None):
         super().__init__(ppal)
         loadUi('opciones_ventana.ui',self)
         self.__ventanaPadre = ppal
         self.setup()
-    
+
+#conecta los botones de la ventana con las funciones para abrir las ventanas hijas
     def setup(self):
         self.leer_archivo_dcm.clicked.connect(self.abrirVentanaBuscarCarpeta)
         self.mirar_inventarios.clicked.connect(self.abrirVentanaInventario)
@@ -91,7 +98,7 @@ class AbrirVentana_opciones(QDialog):
         self.pushButton.clicked.connect(self.abiriVentanaAnestesia)
         self.img3d.clicked.connect(self.abrirVentanaOpciones3d)
         self.log_out.clicked.connect(self.logout)
-
+#itera los botones a las ventanas hijas
     def abiriVentanaAnestesia(self):
         ventana_anestesia=VentanaAnestesia(self)
         self.hide()
@@ -106,7 +113,8 @@ class AbrirVentana_opciones(QDialog):
         ventana_browse=VentanaBuscarCarpeta(self)
         self.hide()
         ventana_browse.show()
-
+        
+# recibe informacion de ventanas hijas para llevarla a la ventana principal y hacer las conexiones con el modelo y controlador
     def recibir_imagen(self,imagen):
         self.__ventanaPadre.recibir_imagen(imagen)
 
@@ -142,14 +150,15 @@ class AbrirVentana_opciones(QDialog):
     def logout(self):
         self.__ventanaPadre.show()
         self.hide()
-
+        
+#se crea la ventana de imagenes y sus funcionalidades
 class VentanaOpciones3d(QDialog):
     def __init__(self, ppal=None):
         super().__init__(ppal)
         loadUi('opciones3d.ui',self)
         self.__ventanaPadre = ppal
         self.setup()
-    
+    # se conectan los botones a la ventana buscar y abirir archivos de imagen
     def setup(self):
         self.dcm.clicked.connect(self.abrirVentanaBuscarCarpeta)
         self.nii.clicked.connect(self.abrirVentanaBuscarArchivo)
@@ -168,7 +177,7 @@ class VentanaOpciones3d(QDialog):
 
     def recibir_imagen3d(self,imagen):
         self.__ventanaPadre.recibir_imagen3d(imagen)
-
+#funcion de cerrar y deslogear
     def cerrar(self):
         self.__ventanaPadre.show()
         self.hide()
@@ -176,7 +185,8 @@ class VentanaOpciones3d(QDialog):
     def logout(self):
         self.__ventanaPadre.logout()
         self.hide()
-
+        
+# clase buscar y visualizar carpetas para nifti
 class VentanaBuscarCarpeta2(QDialog):
     def __init__(self, ppal=None):
         super().__init__(ppal)
@@ -244,7 +254,8 @@ class VentanaBuscarArchivo(QDialog):
     def logout(self):
         self.__ventanaPadre.logout()
         self.hide()
-
+        
+#clase visualizar en 3d
 class VentanaVisualizacion3d(QDialog):
     def __init__(self, ppal=None):
         super().__init__(ppal)
@@ -285,23 +296,25 @@ class VentanaVisualizacion3d(QDialog):
     def logout(self):
         self.__ventanaPadre.logout()
         self.hide()
-
+        
+# clase para la ventana de opciones para personal
 class Ventana_opciones2(QDialog):
     def __init__(self, ppal=None):
         super().__init__(ppal)
         loadUi('opciones_ventana2.ui',self)
         self.__ventanaPadre = ppal
         self.setup()
-    
+    #conexion de botones
     def setup(self):
         self.mirar_inventarios.clicked.connect(self.abrirVentanaInventario)
         self.log_out.clicked.connect(self.logout)
 
+    #funcion de abrir inventarios 
     def abrirVentanaInventario(self):
         ventana_inventario=VentanaInventario(self)
         self.hide()
         ventana_inventario.show() 
-
+    # funcion para cerrar y delogear
     def cerrar(self):
         self.__ventanaPadre.show()
         self.hide()
@@ -309,7 +322,8 @@ class Ventana_opciones2(QDialog):
     def logout(self):
         self.__ventanaPadre.show()
         self.hide()
-
+        
+#clase ventana de diagnosticos
 class VentanaDiagnosticos(QDialog):
     def __init__(self, ppal=None):
         super().__init__(ppal)
@@ -317,33 +331,38 @@ class VentanaDiagnosticos(QDialog):
         self.__ventanaPadre = ppal
         self.setWindowTitle("apertura de diagnosticos")
         self.setup()
-
+    #conexion  de botones
     def setup(self):
         self.open_file.clicked.connect(self.abrir_dialogo)
         self.salir_boton.clicked.connect(self.cerrar)
         self.log_out.clicked.connect(self.logout)
-    
+
+    # funcion de salir
     def cerrar(self):
         self.__ventanaPadre.show()
         self.hide()
-
+    # funcion de abrir y leer archivos txt 
     def abrir_dialogo(self):
-        filename= QFileDialog.getOpenFileName(self,"Open File")[0]
+        filename= QFileDialog.getOpenFileName(self,"Open File")[0] # toma el primer dato de la tupla que es la dirreccion
         self.file_path.setText(filename)
+        #abre en modo leer el archivo txt
         text = open(filename,"r")
+        #muestra el archivo en un textedit
         self.display.setText(text.read())
 
     def logout(self):
         self.__ventanaPadre.logout()
         self.hide()
-
+        
+#clase administracion de anestesia
 class VentanaAnestesia(QDialog):
     def __init__(self, ppal=None):
         super().__init__(ppal)
         loadUi('anestesia.ui',self)
         self.__ventanaPadre = ppal
         self.setup()
-    
+
+    #funcionalidades y conexion de botones
     def setup(self):
         #se programa la senal para el boton
         self.grafica=Canvas_grafica()
@@ -354,7 +373,7 @@ class VentanaAnestesia(QDialog):
         self.boton_salir.clicked.connect(self.cerrar)
         self.log_out.clicked.connect(self.logout)
 
-
+    #funcionalidades de los sliders
     def slider_uno(self, event):
         valor1 = self.__ventanaPadre.slider_uno(event)
         self.grafica.datos1(valor1)
@@ -374,7 +393,8 @@ class VentanaAnestesia(QDialog):
     def cerrar(self):
         self.__ventanaPadre.show()
         self.hide()
-
+        
+#clase que grafica las en la ventana anestesia
 class Canvas_grafica(FigureCanvas):
     def __init__(self, parent=None):     
         self.fig , self.ax = plt.subplots(facecolor='gray')
@@ -394,7 +414,8 @@ class Canvas_grafica(FigureCanvas):
 
     def datos3(self, valor3):
         self.nivel3 = valor3
-        
+    
+    # funcion de graficar    
     def grafica_datos(self):
         plt.title("Administracion de anestesia")
         x = np.arange(-np.pi, 10*np.pi, 0.01) 
@@ -404,7 +425,8 @@ class Canvas_grafica(FigureCanvas):
         line.set_ydata(np.sin(x)+24)
         line2.set_ydata(np.sin(x)+24)
         QtCore.QTimer.singleShot(10, self.grafica_datos)
-    
+
+#clase leer dicoms para medicos   
 class VentanaBuscarCarpeta(QDialog):
     def __init__(self, ppal=None):
         super().__init__(ppal)
@@ -441,7 +463,8 @@ class VentanaBuscarCarpeta(QDialog):
     def logout(self):
         self.__ventanaPadre.logout()
         self.hide()
-
+        
+#clase de visualizacion para medico
 class VentanaVisualizacion(QDialog):
     def __init__(self, ppal=None):
         super().__init__(ppal)
@@ -484,32 +507,35 @@ class VentanaVisualizacion(QDialog):
     def logout(self):
         self.__ventanaPadre.logout()
         self.hide()
-
+        
+#funcion de ver inventario
 class VentanaInventario(QDialog):
     def __init__(self, ppal=None):
         super().__init__(ppal)
         loadUi('inventario.ui',self)
         self.__ventanaPadre = ppal
         self.setup()
-
+#conexion de botones
     def setup(self):
         self.boton_abrir.clicked.connect(self.abrir_archivo)
         self.pushButton.clicked.connect(self.crear_tabla)
         self.boton_salir.clicked.connect(self.salir_archivo)
         self.log_out.clicked.connect(self.logout)
 
+    #funcionalidad abrir archivos
     def abrir_archivo(self):
         #nos da una tupla que es la direccion y un nombre 
         file= QFileDialog.getOpenFileName(self, "abrir archivo Excel","","Excel Files (*.xlsx) ;; All Files (*)" )
         self.direccion = file [0]
-    
+        
+    #funcion para crear la tabla
     def crear_tabla(self):
         try:
             df= pd.read_excel(self.direccion) #leemos el archivo
             columnas = list(df.columns) # crea una lista del encabezado
             df_fila= df.to_numpy().tolist() # crea una lista de listas de todas las filas
-            x=len(columnas)
-            y=len(df_fila)
+            x=len(columnas) #toma el largo de columnas
+            y=len(df_fila)  #toma el largo de filas
 
 #excepciones para formato incorrecto o no hallado
 
@@ -536,7 +562,7 @@ class VentanaInventario(QDialog):
                     dato=""
 #asigna los datos a la columna y fila correspondiente
                 self.tableWidget.setItem(i,j,QTableWidgetItem(dato))
-        
+# funciones de salir       
     def salir_archivo(self):
         self.__ventanaPadre.show()
         self.hide()
